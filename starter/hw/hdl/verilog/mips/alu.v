e55f06ae5a57e41e6e6bfaa314f42492dedc25a3
SWAQ\Stebe <lopez.stephen27@gmail.com>
2018-03-03 17:06:31 -0800

    Eugenios Changes to verlog code

--- starter/hw/hdl/verilog/mips/alu.v
+++ starter/hw/hdl/verilog/mips/alu.v
@@ -29,18 +29,21 @@ module alu (
     always @* begin
         case (alu_opcode)
             // PERFORM ALU OPERATIONS DEFINED ABOVE
-            `ALU_ADD:   alu_result = alu_op_x + alu_op_y;
+            `ALU_ADD:   alu_result = alu_op_x_signed + alu_op_y_signed;
             `ALU_ADDU:  alu_result = alu_op_x + alu_op_y;
             `ALU_AND:   alu_result = alu_op_x & alu_op_y;
+		`ALU_MUL:	alu_result = alu_op_x_signed * alu_op_y_signed;
             `ALU_OR:    alu_result = alu_op_x | alu_op_y;
-            `ALU_SUB:   alu_result = alu_op_x - alu_op_y;
+            `ALU_SUB:   alu_result = alu_op_x_signed - alu_op_y_signed;
             `ALU_SUBU:  alu_result = alu_op_x - alu_op_y;
             `ALU_SLTU:  alu_result = alu_op_x < alu_op_y;
             `ALU_SLT:   alu_result = alu_op_x_signed < alu_op_y_signed;
             `ALU_SRL:   alu_result = alu_op_y >> alu_op_x[4:0]; // shift operations are Y >> X
+		`ALU_SRA:	alu_result = alu_op_y_signed >>> alu_op_x_signed;
             `ALU_SLL:   alu_result = alu_op_y << alu_op_x[4:0];
             `ALU_PASSX: alu_result = alu_op_x;
             `ALU_PASSY: alu_result = alu_op_y;
+		`ALU_XOR:	alu_result = alu_op_x ^ alu_op_y;
             default:    alu_result = 32'hxxxxxxxx;   // undefined
         endcase
     end
